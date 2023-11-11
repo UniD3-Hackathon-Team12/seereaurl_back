b9de8a93ea722b0aa583e5e52bc79d89446a892a
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-11-12 01:36:56 +0900

    get_top5view

--- src/main/java/com/example/SeaReaUrl_back/FraudController.java
+++ src/main/java/com/example/SeaReaUrl_back/FraudController.java
@@ -17,37 +17,37 @@ public class FraudController {
     public Top5Response getTop5AccountView(){
         return fraudService.getTop5AccountView();
     }
-    @GetMapping("/url/view")
-    public Top5Response getTop5UrlView(){
-        return fraudService.getTop5UrlView();
-    }
-    @GetMapping("/account/report")
-    public Top5Response getTop5AccountReports(){
-        return fraudService.getTop5AccountReports();
-    }
-    @GetMapping("/url/report")
-    public Top5Response getTop5UrlReports(){
-        return fraudService.getTop5UrlReports();
-    }
-
-    @PostMapping("/report/account")
-    public StringResponse reportAccount(){
-        return fraudService.reportAccount();
-    }
-    @PostMapping("/report/url")
-    public StringResponse reportUrl(){
-        return fraudService.reportUrl();
-    }
-
-    @GetMapping("/checkUrl")
-    public IsFraudUrlResponse checkUrlValidation(@RequestParam String url){
-        return fraudService.checkUrlValidation(url);
-    }
-
-    @GetMapping("/checkAccount")
-    public IsFraudAccountResponse checkAccountValidation(@RequestParam String accountName){
-        return fraudService.checkAccountValidation(accountName);
-    }
+//    @GetMapping("/url/view")
+//    public Top5Response getTop5UrlView(){
+//        return fraudService.getTop5UrlView();
+//    }
+//    @GetMapping("/account/report")
+//    public Top5Response getTop5AccountReports(){
+//        return fraudService.getTop5AccountReports();
+//    }
+//    @GetMapping("/url/report")
+//    public Top5Response getTop5UrlReports(){
+//        return fraudService.getTop5UrlReports();
+//    }
+//
+//    @PostMapping("/report/account")
+//    public StringResponse reportAccount(){
+//        return fraudService.reportAccount();
+//    }
+//    @PostMapping("/report/url")
+//    public StringResponse reportUrl(){
+//        return fraudService.reportUrl();
+//    }
+//
+//    @GetMapping("/checkUrl")
+//    public IsFraudUrlResponse checkUrlValidation(@RequestParam String url){
+//        return fraudService.checkUrlValidation(url);
+//    }
+//
+//    @GetMapping("/checkAccount")
+//    public IsFraudAccountResponse checkAccountValidation(@RequestParam String accountName){
+//        return fraudService.checkAccountValidation(accountName);
+//    }
 
 
 
--- src/main/java/com/example/SeaReaUrl_back/Top5Response.java
+++ src/main/java/com/example/SeaReaUrl_back/Top5Response.java
@@ -1,4 +1,43 @@
 package com.example.SeaReaUrl_back;
 
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Builder
 public class Top5Response {
+
+    private List<Top5AccountResponse> top5Accounts;
+
+    public Top5Response(List<Top5AccountResponse> top5Accounts) {
+        this.top5Accounts = top5Accounts;
+    }
+
+//    private List<FraudAccount> top5Accounts;
+//
+//    public Top5Response(List<FraudAccount> top5Accounts) {
+//        this.top5Accounts = top5Accounts;
+//    }
+//
+//    public List<FraudAccount> getTop5Accounts() {
+//        return top5Accounts;
+//    }
+//    private Long id;
+//    private String url;
+//    private Boolean IsFraud;
+//    private Long view;
+//    private Long report;
+//
+//    public static Top5Response {
+//        return Top5Response.builder()
+//                .id()
+//                .build();
+//    }
+
 }
